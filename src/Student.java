7ee7bccaa9c9fcb733361a21a6f19013cf5cebd2
nikhila-m <nikhilasrinivas80@gmail.com>
2017-09-08 15:25:13 +0530

    Add files via upload

--- src/Student.java
+++ src/Student.java
@@ -1,92 +1,74 @@
 import java.util.Date;
 
-/**
- * Class that stores information about the concrete student
- * 
- * DO NOT REMOVE given methods, but you can add new methods/fields/constructor
- * and change the given methods implementation. For example you can change implementation for equals()
- * or hashCode() method
- * 
- */
 public class Student implements Comparable {
 
-	/**
-	 * student id
-	 */
 	private int id;
-
-	/**
-	 * student name and surname separated by the whitespace for example:
-	 * fullName = "David Luis";
-	 */
 	private String fullName;
-
-	/**
-	 * student date of birth in "yyyy-MM-dd" format
-	 */
 	private Date birthDate;
-
-	/**
-	 * student average mark
-	 */
 	private double avgMark;
 
 	public Student(int id, String fullName, Date birthDate, double avgMark) {
-		this.id = id;
-		this.fullName = fullName;
-		this.birthDate = birthDate;
-		this.avgMark = avgMark;
+           this.id = id;
+		   this.fullName = fullName;
+		   this.birthDate = birthDate;
+		   this.avgMark = avgMark;
 	}
 
 	public int getId() {
-		return id;
+           return this.id;
 	}
 
 	public void setId(int id) {
-		this.id = id;
+         this.id = id;
 	}
 
 	public String getFullName() {
-		return fullName;
+          return fullName;
 	}
 
 	public void setFullName(String fullName) {
-		this.fullName = fullName;
+           this.fullName = fullName;
 	}
 
 	public Date getBirthDate() {
-		return birthDate;
+        return this.birthDate;
 	}
 
 	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
+          this.birthDate = birthDate;
 	}
 
 	public double getAvgMark() {
-		return avgMark;
+          return avgMark;
 	}
 
 	public void setAvgMark(double avgMark) {
-		this.avgMark = avgMark;
+         this.avgMark = avgMark;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+         if (!(o instanceof Student)) {
+            return false;
+        }
+		Student s = (Student)o;
+		return this.getId() == s.getId() && this.getFullName() == s.getFullName() && this.getAvgMark() == s.getAvgMark() && 
+			   this.getBirthDate().compareTo(s.getBirthDate()) == 0;
 	}
 
 	@Override
 	public int hashCode() {
-		return super.hashCode();
+          return this.getId();
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		return super.equals(obj);
-	}
-	
-	
-	/**
-	 * DO NOT change this method it will be used during the task check
-	 */
-	@Override
 	public int compareTo(Object o) {
-		Student other = (Student) o;
-		return (this.fullName.compareTo(other.fullName));
+         if (!(o instanceof Student)) {
+            return -1;
+        }
+		Student s = (Student)o;
+          if(this.getId() < s.getId()) return -1;
+		  else if(this.getId() > s.getId()) return 1;
+		  else return 0;
 	}
 }
